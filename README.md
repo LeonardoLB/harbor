9d78d2a3f6fcb6ce5f09f78ef1ac464171c7608b
Leonardo Lemes Bufalo <leobufalo@Leonardos-MacBook-Air.local>
2018-11-17 18:56:48 -0200

    done gulp file and adjust main template pug and stylus

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # harbor
-A structure to make more easy your development and for create a standard template for web pages
+A structure to make more easy your development and for create a standard template for web pages development.
+
+
+recommended requirements
+
+* Apache/2.4.29 or greater version;
+* Python3
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,38 @@
+//----------//
+//-- VARS --//
+//----------//
+
+
 var gulp = require( 'gulp' );
 var pug = require( 'gulp-pug' );
-// var pug = require('gulp-stylus');
+var stylus = require('gulp-stylus');
 
 
-// TASKS
+
+//-----------//
+//-- TASKS --//
+//-----------//
+
 gulp.task( 'pug', function(){
 
     return gulp.src( 'src/views/pages/*.pug' )
         .pipe( pug() )
-        .pipe( gulp.dest( 'views' ));
+        .pipe( gulp.dest( 'views/' ));
 });
+
+gulp.task('stylus', function () {
+
+    return gulp.src('src/styles/main.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('assets/css/'));
+});
+
 
 // WATCH
 gulp.task( 'watch', function(){
 
     gulp.watch( 'src/views/pages/*', ['pug'] )
+    gulp.watch( 'src/styles/**/*', ['stylus'] )
 
 })
 
